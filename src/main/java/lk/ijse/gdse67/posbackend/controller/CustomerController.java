298744eb78f26b1639a16dfaad862d96852114a1
KusalDemo <kusalgunasekara2002@gmail.com>
2024-08-19 11:17:17 +0530

    Implemented doPost in CustomerController

--- src/main/java/lk/ijse/gdse67/posbackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse67/posbackend/controller/CustomerController.java
@@ -6,6 +6,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.gdse67.posbackend.bo.BoFactory;
+import lk.ijse.gdse67.posbackend.bo.custom.CustomerBo;
 import lk.ijse.gdse67.posbackend.dto.CustomerDto;
 import lk.ijse.gdse67.posbackend.entity.Customer;
 import org.slf4j.Logger;
@@ -18,10 +20,11 @@ import java.io.Writer;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@WebServlet(urlPatterns = "/customer")
+@WebServlet(urlPatterns = "/customer",loadOnStartup = 2)
 public class CustomerController extends HttpServlet {
     Connection connection;
     static Logger logger = LoggerFactory.getLogger(CustomerController.class);
+    CustomerBo customerBo=(CustomerBo) BoFactory.getBoFactory().getBo(BoFactory.BoFactoryTypes.CUSTOMER);
     @Override
     public void init() {
         logger.info("Customer Controller Initialized");
@@ -30,7 +33,7 @@ public class CustomerController extends HttpServlet {
             DataSource pool =(DataSource) ctx.lookup("java:comp/env/jdbc/aadJavaeePos");
             this.connection = pool.getConnection();
         } catch (NamingException|SQLException e) {
-            logger.error("Error while connecting to database : ",e.getMessage());
+            logger.error("Error while connecting to database : ",e);
             throw new RuntimeException(e);
         }
     }
@@ -43,7 +46,12 @@ public class CustomerController extends HttpServlet {
         try(Writer writer=response.getWriter()){
             Jsonb jsonb = JsonbBuilder.create();
             CustomerDto customerDto = jsonb.fromJson(request.getReader(), CustomerDto.class);
+            System.out.println(customerDto.getName()+" , "+customerDto.getEmail());
+            boolean isSaved = customerBo.saveCustomer(customerDto);
+            jsonb.toJson(isSaved,writer);
+            logger.info("Customer saving status : isSaved? - "+isSaved);
         }catch (Exception e){
+            logger.error("Error while saving customer : ",e);
             throw new RuntimeException(e);
         }
     }
