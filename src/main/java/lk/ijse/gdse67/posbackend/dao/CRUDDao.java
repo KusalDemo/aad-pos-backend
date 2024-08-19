668a1e18aaa6e8b63f3e4ca43e63bbfc6c9e4a62
KusalDemo <kusalgunasekara2002@gmail.com>
2024-08-19 11:03:48 +0530

    Implemented CRUDDao , SuperDao

---
+++ src/main/java/lk/ijse/gdse67/posbackend/dao/CRUDDao.java
@@ -0,0 +1,18 @@
+package lk.ijse.gdse67.posbackend.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CRUDDao<T> extends SuperDao {
+
+    T search(String id) throws SQLException;
+
+    boolean save(T t) throws SQLException, ClassNotFoundException;
+
+    boolean update(T t) throws SQLException;
+
+    boolean delete(String id) throws SQLException;
+
+    ArrayList<T> getAll() throws SQLException;
+
+}
---
+++ src/main/java/lk/ijse/gdse67/posbackend/dao/SuperDao.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse67.posbackend.dao;
+
+public interface SuperDao {
+}
